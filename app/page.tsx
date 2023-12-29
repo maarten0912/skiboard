ae81baeeb9589b99c2da79d7d7f822240609f826
Maarten Meijer <maarten0912@gmail.com>
2023-12-29 23:13:52 +0100

    First version

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,222 @@
-import Image from 'next/image'
+'use client'
+
+import jsonData from './data.json'
+import googleSecret from '../google-secret.json'
+import React, { useState, useEffect } from 'react'
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import { JWT } from 'google-auth-library';
+
+
+// Auth for Sheets
+const SCOPES = [
+  'https://www.googleapis.com/auth/spreadsheets',
+  'https://www.googleapis.com/auth/drive.file',
+];
+
+const jwt = new JWT({
+  email: googleSecret.client_email,
+  key: googleSecret.private_key,
+  scopes: SCOPES,
+});
+
+// Sheets config variables
+const SPREADSHEET_ID = "1-BlGsmxcBeYy4PebLfrRTBdpfqUh7kwx8rrBx0uauwc"
+const SHEET_ID = "405578724"
+
+const doc = new GoogleSpreadsheet(SPREADSHEET_ID, jwt)
 
 export default function Home() {
+  
+  enum Color {
+    GREEN = 'green',
+    BLUE = 'blue',
+    RED = 'red',
+    BLACK = 'black'
+  }
+
+  const colorOrder = Object.values(Color);
+
+  const stringToCss = {
+    "green": "bg-green-600",
+    "blue": "bg-blue-600",
+    "red": "bg-red-600",
+    "black": "bg-gray-800"
+  }
+
+  interface Slope {
+    name: string,
+    color: Color,
+    area: string,
+    zone: string,
+    spreadSheetRow?: any
+  }
+
+  const [greenFilter, setGreenFilter] = useState(false)
+  const [blueFilter, setBlueFilter] = useState(false)
+  const [redFilter, setRedFilter] = useState(false)
+  const [blackFilter, setBlackFilter] = useState(false)
+
+  const [areaFilter, setAreaFilter] = useState("none")
+  const [zoneFilter, setZoneFilter] = useState("none")
+
+  const [slopes, setSlopes] = useState<Slope[]>([])
+  const [finishedSlopes, setFinishedSlopes] = useState<Slope[]>([])
+
+  const [user, setUser] = useState("")
+
+  useEffect(() => {
+    doc.loadInfo().then(async () => {
+      const rows = await doc.sheetsById[SHEET_ID].getRows()
+      const slopesData: Slope[] = rows.filter(row => row.get(user) == 'FALSE').map(row => ({
+        name: row.get('Name'),
+        area: row.get('Area'),
+        zone: row.get('Zone'),
+        color: row.get('Color'),
+        spreadSheetRow: row
+      }))
+
+      slopesData.sort(slopeSort)
+      setSlopes(slopesData);
+
+      const finishedSlopesData: Slope[] = rows.filter(row => row.get(user) == 'TRUE').map(row => ({
+        name: row.get('Name'),
+        area: row.get('Area'),
+        zone: row.get('Zone'),
+        color: row.get('Color'),
+        spreadSheetRow: row
+      }))
+
+      finishedSlopesData.sort(slopeSort)
+      setFinishedSlopes(finishedSlopesData);
+
+    })
+  }, [user])
+
+  function slopeSort(a: Slope, b: Slope) {
+    return b.area.localeCompare(a.area) || a.zone.localeCompare(b.zone) || colorOrder.indexOf(a.color) - colorOrder.indexOf(b.color) || a.name.localeCompare(b.name)
+  }
+
+  function colorFilterFunction(element: { color: string }) {
+    if (!greenFilter && !blueFilter && !redFilter && !blackFilter) {
+      return true
+    }
+
+    if (greenFilter && element.color == "green") {
+      return true
+    }
+    if (blueFilter && element.color == "blue") {
+      return true
+    }
+    if (redFilter && element.color == "red") {
+      return true
+    }
+    if (blackFilter && element.color == "black") {
+      return true
+    }
+
+    return false
+  }
+
+  function areaFilterFunction(element: {area: string}) {
+    return areaFilter == "none" || areaFilter == element.area
+  }
+
+  function zoneFilterFunction(element: {zone: string}) {
+    return zoneFilter == "none" || zoneFilter == element.zone
+  }
+
+  async function clickSlopeFunction(slope: Slope) {
+    slope.spreadSheetRow.assign({
+      [user]: "TRUE"
+    })
+    await slope.spreadSheetRow.save()
+    setSlopes(slopes.filter(e => e != slope))
+    setFinishedSlopes([...finishedSlopes, slope].sort(slopeSort))
+  }
+
+  async function clickFinishedSlopeFunction(slope: Slope) {
+    slope.spreadSheetRow.assign({
+      [user]: "FALSE"
+    })
+    await slope.spreadSheetRow.save()
+    setFinishedSlopes(finishedSlopes.filter(e => e != slope))
+    setSlopes([...slopes, slope].sort(slopeSort))
+  }
+
+  function setUserFunction(selection: string) {
+    setUser(selection)
+  }
+
+  const zones = [
+    "Arc 1600",
+    "Arc 1800",
+    "Arc 2000",
+    "Peisey Vallandry",
+    "Villarogers",
+    "Altitude Domain",
+    "Champagny en Vanoise",
+    "Plagne Montalbert",
+    "Montchavin - Les Coches"
+  ]
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex font-sans min-h-screen flex-col items-center p-2">
+
+      <select name={areaFilter} onChange={e => setUserFunction(e.target.value)} className="font-bold py-1 px-2 m-1 rounded w-full">
+        <option value="">Ik ben...</option>
+        <option value="Brigitte">Brigitte</option>
+        <option value="Chris">Chris</option>
+        <option value="Karianne">Karianne</option>
+        <option value="Christiaan">Christiaan</option>
+        <option value="Sifra">Sifra</option>
+        <option value="Maarten">Maarten</option>
+        <option value="Daniël">Daniël</option>
+        <option value="Pepijn">Pepijn</option>
+        <option value="Joëlle">Joëlle</option>
+        <option value="Inge">Inge</option>
+        <option value="Melle">Melle</option>
+      </select>
+
+      {/* Only display page if username is set */}
+
+      {user == "" ? null :
+        <div>
+          <div className="flex flex-row justify-between">
+            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 m-1 rounded" onClick={() => setGreenFilter(!greenFilter)}>Groen</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 m-1 rounded" onClick={() => setBlueFilter(!blueFilter)}>Blauw</button>
+            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 m-1 rounded" onClick={() => setRedFilter(!redFilter)}>Rood</button>
+            <button className="bg-gray-800 hover:bg-black text-white font-bold py-1 px-2 m-1 rounded" onClick={() => setBlackFilter(!blackFilter)}>Zwart</button>
+          </div>
+          <select name={areaFilter} onChange={e => setAreaFilter(e.target.value)} className="font-bold py-1 px-2 m-1 rounded w-full">
+            <option value="none">Filter een gebied...</option>
+            <option value="Les Arcs">Les Arcs</option>
+            <option value="La Plagne">La Plagne</option>
+          </select>
+          <select name={zoneFilter} onChange={e => setZoneFilter(e.target.value)} className="font-bold py-1 px-2 m-1 rounded w-full">
+            <option value="none">Filter een zone...</option>
+            {zones.map((item, index) => (
+              <option value={item} key={index}>{item}</option>
+            ))}
+          </select>
         </div>
-      </div>
+      }
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {user != "" && slopes.filter(e => colorFilterFunction(e) && areaFilterFunction(e) && zoneFilterFunction(e)).map((item, index) => (
+        <button className="flex flex-row my-2 p-1 hover:bg-yellow-100 rounded-lg" key={index} onClick={() => clickSlopeFunction(item)}>
+          <div className={`w-4 h-4 self-center rounded-full px-1 ${stringToCss[item.color]}`}></div>
+          <div className="text-2xl self-center font-bold px-1">{item.name}</div>
+          <div className="text-sm italic px-1 self-start">{item.zone}</div>
+        </button>
+      ))}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {user != "" && finishedSlopes.filter(e => colorFilterFunction(e) && areaFilterFunction(e) && zoneFilterFunction(e)).map((item, index) => (
+        <button className="flex flex-row my-2 p-1 bg-yellow-200 hover:bg-yellow-400 rounded-lg" key={index} onClick={() => clickFinishedSlopeFunction(item)}>
+          <div className={`w-4 h-4 self-center rounded-full px-1 ${stringToCss[item.color]}`}></div>
+          <div className="text-2xl self-center font-bold px-1">{item.name}</div>
+          <div className="text-sm italic px-1 self-start">{item.zone}</div>
+        </button>
+      ))}
+      
     </main>
   )
 }
