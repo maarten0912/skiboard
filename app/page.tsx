e8289477eacea52cdcb387689fc3f718a3807e97
Maarten Meijer <maarten0912@gmail.com>
2024-01-01 18:20:39 +0100

    Zoeken en het kan meerdere mensen aan

--- app/page.tsx
+++ app/page.tsx
@@ -43,12 +43,27 @@ export default function Home() {
     "black": "bg-gray-800"
   }
 
+  const userColoms: {[key: string]: string} = {
+    "Pepijn": 'E',
+    "Maarten": 'F',
+    "Chris": 'G',
+    "Brigitte": 'H',
+    "Joëlle": 'I',
+    "Daniël": 'J',
+    "Sifra": 'K',
+    "Karianne": 'L',
+    "Christiaan": 'M',
+    "Melle": 'N',
+    "Inge": 'O'
+  }
+    
+
   interface Slope {
     name: string,
     color: Color,
     area: string,
     zone: string,
-    spreadSheetRow?: any
+    slopeIndex: number
   }
 
   const [greenFilter, setGreenFilter] = useState(false)
@@ -58,6 +73,7 @@ export default function Home() {
 
   const [areaFilter, setAreaFilter] = useState("none")
   const [zoneFilter, setZoneFilter] = useState("none")
+  const [inputFilter, setInputFilter] = useState("")
 
   const [slopes, setSlopes] = useState<Slope[]>([])
   const [finishedSlopes, setFinishedSlopes] = useState<Slope[]>([])
@@ -66,29 +82,33 @@ export default function Home() {
   const [userScore, setUserScore] = useState(0)
 
   useEffect(() => {
+    if (user == "") {
+      return
+    }
     doc.loadInfo().then(async () => {
       const rows = await doc.sheetsById[SHEET_ID].getRows()
-      const slopesData: Slope[] = rows.filter(row => row.get(user) == 'FALSE').map(row => ({
+      const slopesData: Slope[] = rows.map((row, index) => ({
         name: row.get('Name'),
         area: row.get('Area'),
         zone: row.get('Zone'),
         color: row.get('Color'),
-        spreadSheetRow: row
+        slopeIndex: index
       }))
 
+      // alert(slopesData[slopesData.length - 1].slopeIndex)
       slopesData.sort(slopeSort)
-      setSlopes(slopesData);
 
-      const finishedSlopesData: Slope[] = rows.filter(row => row.get(user) == 'TRUE').map(row => ({
-        name: row.get('Name'),
-        area: row.get('Area'),
-        zone: row.get('Zone'),
-        color: row.get('Color'),
-        spreadSheetRow: row
-      }))
-
-      finishedSlopesData.sort(slopeSort)
-      setFinishedSlopes(finishedSlopesData);
+      await doc.sheetsById[SHEET_ID].loadCells(userColoms[user] + '2:' + userColoms[user] + '261')
+      
+      const finishedSlopesData: Slope[] = []
+      slopesData.forEach(slope => {
+        if (doc.sheetsById[SHEET_ID].getCellByA1(userColoms[user] + (slope.slopeIndex + 2)).value) {
+          finishedSlopesData.push(slope)
+        }
+      })
+      
+      setSlopes(slopesData)
+      setFinishedSlopes(finishedSlopesData)
 
     })
   }, [user])
@@ -133,20 +153,22 @@ export default function Home() {
     return zoneFilter == "none" || zoneFilter == element.zone
   }
 
+  function inputFilterFunction(element: {name: string}) {
+    return element.name.toLowerCase().includes(inputFilter.toLowerCase())
+  }
+
   async function clickSlopeFunction(slope: Slope) {
-    slope.spreadSheetRow.assign({
-      [user]: "TRUE"
-    })
-    await slope.spreadSheetRow.save()
+    doc.sheetsById[SHEET_ID].getCellByA1(userColoms[user] + (slope.slopeIndex + 2)).value = true
+    await doc.sheetsById[SHEET_ID].saveUpdatedCells()
+
     setSlopes(slopes.filter(e => e != slope))
     setFinishedSlopes([...finishedSlopes, slope].sort(slopeSort))
   }
 
   async function clickFinishedSlopeFunction(slope: Slope) {
-    slope.spreadSheetRow.assign({
-      [user]: "FALSE"
-    })
-    await slope.spreadSheetRow.save()
+    doc.sheetsById[SHEET_ID].getCellByA1(userColoms[user] + (slope.slopeIndex + 2)).value = false
+    await doc.sheetsById[SHEET_ID].saveUpdatedCells()
+
     setFinishedSlopes(finishedSlopes.filter(e => e != slope))
     setSlopes([...slopes, slope].sort(slopeSort))
   }
@@ -176,7 +198,7 @@ export default function Home() {
   return (
     <main className="flex font-sans min-h-screen flex-col items-center p-2">
 
-      <select name={areaFilter} onChange={e => setUserFunction(e.target.value)} className="font-bold bg-gray-100 dark:bg-gray-700 py-1 px-2 m-1 rounded w-full">
+      <select onChange={e => setUserFunction(e.target.value)} className="font-bold bg-gray-100 dark:bg-gray-700 py-1 px-2 m-1 rounded w-full">
         <option value="">Ik ben...</option>
         <option value="Brigitte">Brigitte</option>
         <option value="Chris">Chris</option>
@@ -195,19 +217,19 @@ export default function Home() {
 
       {user == "" ? null :
         <div>
+          <div className="flex items-center justify-center font-bold h-10 mx-24 my-1 rounded-full border-2 border-yellow-500 text-yellow-500 min-w-10">{userScore}</div>
           <div className="flex flex-row justify-between">
             <button className={`font-bold py-1 px-2 m-1 rounded border-2 border-green-600 ${greenFilter ? "bg-green-600 hover:border-green-800 hover:bg-green-800 text-white" : "text-green-600 hover:text-green-800 hover:border-green-800"}`} onClick={() => setGreenFilter(!greenFilter)}>Groen</button>
             <button className={`font-bold py-1 px-2 m-1 rounded border-2 border-blue-600 ${blueFilter ? "bg-blue-600 hover:border-blue-800 hover:bg-blue-800 text-white" : "text-blue-600 hover:text-blue-800 hover:border-blue-800"}`} onClick={() => setBlueFilter(!blueFilter)}>Blauw</button>
             <button className={`font-bold py-1 px-2 m-1 rounded border-2 border-red-600 ${redFilter ? "bg-red-600 hover:border-red-800 hover:bg-red-800 text-white" : "text-red-600 hover:text-red-800 hover:border-red-800"}`} onClick={() => setRedFilter(!redFilter)}>Rood</button>
             <button className={`font-bold py-1 px-2 m-1 rounded border-2 border-gray-800 dark:border-gray-600 ${blackFilter ? "bg-gray-800 dark:bg-gray-600 hover:border-black dark:hover:border-gray-800 hover:bg-black dark:hover:bg-gray-800 text-white" : "text-gray-800 dark:text-gray-600 hover:text-black dark:hover:text-gray-800 hover:border-black dark:hover:border-gray-800"}`} onClick={() => setBlackFilter(!blackFilter)}>Zwart</button>
-            <div className="flex items-center justify-center font-bold absolute right-0 w-auto h-10 mx-5 rounded-full border-2 border-yellow-500 text-yellow-500 px-2 min-w-10">{userScore}</div>
           </div>
-          <select name={areaFilter} onChange={e => setAreaFilter(e.target.value)} className="font-bold bg-gray-100 dark:bg-gray-700 py-1 px-2 m-1 rounded w-full">
+          <select onChange={e => setAreaFilter(e.target.value)} className="font-bold bg-gray-100 dark:bg-gray-700 py-1 px-2 m-1 rounded w-full">
             <option value="none">Filter een gebied...</option>
             <option value="Les Arcs">Les Arcs</option>
             <option value="La Plagne">La Plagne</option>
           </select>
-          <select name={zoneFilter} onChange={e => setZoneFilter(e.target.value)} className="font-bold bg-gray-100 dark:bg-gray-700 py-1 px-2 m-1 rounded w-full">
+          <select onChange={e => setZoneFilter(e.target.value)} className="font-bold bg-gray-100 dark:bg-gray-700 py-1 px-2 m-1 rounded w-full">
             <option value="none">Filter een zone...</option>
             {(areaFilter == "none" || areaFilter == 'Les Arcs') && zonesLesArc.map((item, index) => (
               <option value={item} key={index}>{item}</option>
@@ -215,12 +237,12 @@ export default function Home() {
             {(areaFilter == "none" || areaFilter == 'La Plagne') && zonesLaPlagne.map((item, index) => (
               <option value={item} key={index}>{item}</option>
             ))}
-
           </select>
+          <input placeholder="Zoeken..." className="font-bold bg-gray-100 dark:bg-gray-700 py-1 px-2 m-1 rounded w-full" onChange={e => setInputFilter(e.target.value)} />
         </div>
       }
 
-      {user != "" && slopes.filter(e => colorFilterFunction(e) && areaFilterFunction(e) && zoneFilterFunction(e)).map((item, index) => (
+      {user != "" && slopes.filter(e => colorFilterFunction(e) && areaFilterFunction(e) && zoneFilterFunction(e) && inputFilterFunction(e) && !finishedSlopes.includes(e)).map((item, index) => (
         <button className="flex flex-row my-2 p-1 hover:bg-yellow-100 hover:text-black rounded-lg" key={index} onClick={() => clickSlopeFunction(item)}>
           <div className={`w-4 h-4 self-center rounded-full px-1 ${stringToCss[item.color]}`}></div>
           <div className="text-2xl self-center font-bold px-1">{item.name}</div>
@@ -228,7 +250,7 @@ export default function Home() {
         </button>
       ))}
 
-      {user != "" && finishedSlopes.filter(e => colorFilterFunction(e) && areaFilterFunction(e) && zoneFilterFunction(e)).map((item, index) => (
+      {user != "" && finishedSlopes.filter(e => colorFilterFunction(e) && areaFilterFunction(e) && zoneFilterFunction(e) && inputFilterFunction(e)).map((item, index) => (
         <button className="flex flex-row my-2 p-1 bg-yellow-200 hover:bg-yellow-400 text-black rounded-lg" key={index} onClick={() => clickFinishedSlopeFunction(item)}>
           <div className={`w-4 h-4 self-center rounded-full px-1 ${stringToCss[item.color]}`}></div>
           <div className="text-2xl self-center font-bold px-1">{item.name}</div>
